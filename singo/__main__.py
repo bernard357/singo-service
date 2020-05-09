86699dd9fdd2917428063c11862e478b1a1a33a9
Bernard Paques <paqueb@amazon.com>
2020-05-09 23:03:15 +0200

    add capability to bind on address and port set by environment variables SINGO_ADDRESS and SINGO_PORT

--- singo/__main__.py
+++ singo/__main__.py
@@ -1,4 +1,8 @@
+import os
+
 from api import app
 
 
-app.run()
+host_binding = os.environ.get('SINGO_ADDRESS', '0.0.0.0')
+host_port = os.environ.get('SINGO_PORT', '5000')
+app.run(host=host_binding, port=int(host_port))
